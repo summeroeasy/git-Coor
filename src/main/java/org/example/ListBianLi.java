9589ec6c20e21e88d090bd3edf30ea7da4ab1e74
summeroeasy <70423885+summeroeasy@users.noreply.github.com>
2024-11-10 15:16:20 +0800

    Update ListBianLi.java
    删除注释

--- src/main/java/org/example/ListBianLi.java
+++ src/main/java/org/example/ListBianLi.java
@@ -6,20 +6,20 @@ import java.util.List;
 
 public class ListBianLi {
     public static void main(String[] args) {
-//        List<Integer> list = new ArrayList<>();
-//        list.add(1);
-//        list.add(2);
-//        list.add(3);
-//        list.add(4);
-//        list.add(5);
-//        list.add(7);
-//        Iterator<Integer> iterator = list.iterator();
-//        while (iterator.hasNext()){
-//            Integer next = iterator.next();
-//            System.out.println(next);
-//            iterator.remove();
-//        }
-//        System.out.println(list);
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(7);
+       Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()){
+            Integer next = iterator.next();
+            System.out.println(next);
+            iterator.remove();
+        }
+        System.out.println(list);
         System.out.println(5&3);
         System.out.println(2<<3);
         System.out.println(3<<3);
